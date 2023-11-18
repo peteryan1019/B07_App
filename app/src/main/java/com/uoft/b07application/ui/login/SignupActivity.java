5a1bff30d704e176726c7dff01d52b201015d20d
Leo Yang <leoyang@Leos-MBP.ht.home>
2023-11-18 15:54:00 -0500

    Created Sign Up page and its UI, overhauled Sign In page and its UI

---
+++ app/src/main/java/com/uoft/b07application/ui/login/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.uoft.b07application.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uoft.b07application.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
