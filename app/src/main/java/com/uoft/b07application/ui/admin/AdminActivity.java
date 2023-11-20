f39ddd5bd06512777d61e75bfb9fefec0925a313
Bomb3077 <yusheng3077@gmail.com>
2023-11-20 11:30:11 -0500

    rendered sidebar and header into adminPage

--- app/src/main/java/com/uoft/b07application/ui/admin/AdminActivity.java
+++ app/src/main/java/com/uoft/b07application/ui/admin/AdminActivity.java
@@ -1,16 +1,22 @@
 package com.uoft.b07application.ui.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
 import com.uoft.b07application.R;
 
 public class AdminActivity extends AppCompatActivity {
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+//        drawerLayout = findViewById(R.id.nav_admin_view);
     }
 }
