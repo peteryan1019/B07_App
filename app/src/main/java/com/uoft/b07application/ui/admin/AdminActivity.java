abae942966f8ea19be0fc383619f82e7bc348902
Bomb3077 <yusheng3077@gmail.com>
2023-11-21 09:50:18 -0500

    make menu clickable

--- app/src/main/java/com/uoft/b07application/ui/admin/AdminActivity.java
+++ app/src/main/java/com/uoft/b07application/ui/admin/AdminActivity.java
@@ -1,22 +1,65 @@
 package com.uoft.b07application.ui.admin;
 
+import androidx.activity.ComponentActivity;
+import androidx.activity.OnBackPressedDispatcher;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.window.OnBackInvokedDispatcher;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 import com.uoft.b07application.R;
 
-public class AdminActivity extends AppCompatActivity {
+public class AdminActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private ComponentActivity componentActivity;
+    //to handle onBackPressedDispatcher method for menu
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
-//        drawerLayout = findViewById(R.id.nav_admin_view);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_admin_view);
+        toolbar = findViewById(R.id.admin_toolbar);
+
+        setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.admin_navigation_drawer_open, R.string.admin_navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
+    public void menuOnPress(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            OnBackPressedDispatcher onBackPressedDispatcher = componentActivity.getOnBackPressedDispatcher();
+            onBackPressedDispatcher.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
+
+
+    // Add the callback to the OnBackPressedDispatcher
 }
