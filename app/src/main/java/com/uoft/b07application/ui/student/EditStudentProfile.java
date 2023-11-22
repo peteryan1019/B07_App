01f3d98f737789fa29e44808585bf85d1a5fb76a
edwardz271 <144949451+edwardz271@users.noreply.github.com>
2023-11-22 15:12:05 -0500

    some student page functionalities

---
+++ app/src/main/java/com/uoft/b07application/ui/student/EditStudentProfile.java
@@ -0,0 +1,16 @@
+package com.uoft.b07application.ui.student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uoft.b07application.R;
+
+public class EditStudentProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_student_profile);
+    }
+}
