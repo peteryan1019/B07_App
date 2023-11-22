e14cf5508490e2f945550dc11fcb0babb3314d17
Ben Lu <lenbu@Bens-MacBook-Pro.local>
2023-11-22 14:31:45 -0500

    updated student feedback page

---
+++ app/src/main/java/com/uoft/b07application/ui/BenStudentPageEvents/EventPageActivity.java
@@ -0,0 +1,14 @@
+package com.uoft.b07application.ui.BenStudentPageEvents;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.uoft.b07application.R;
+
+public class EventPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_page);
+    }
+}
--- app/src/main/java/com/uoft/b07application/ui/BenStudentPageEvents/StudentFeedback.java
+++ app/src/main/java/com/uoft/b07application/ui/BenStudentPageEvents/StudentFeedback.java
@@ -1,2 +1,50 @@
-package com.uoft.b07application.ui.BenStudentPageEvents;public class StudentFeedback {
+package com.uoft.b07application.ui.BenStudentPageEvents;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+//need to import the layout stuff
+import com.uoft.b07application.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class StudentFeedback extends AppCompatActivity{
+    private EditText commentEditText;
+    private TextView feedbackPromptTextView;
+    private TextView thankYouMessageTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_feedback_layout);
+
+        // Initialize UI elements
+        commentEditText = findViewById(R.id.commentEditText);
+        Button submitButton = findViewById(R.id.submitButton);
+        feedbackPromptTextView = findViewById(R.id.feedbackPromptTextView);
+        thankYouMessageTextView = null;
+
+        // Set click listener for when user submits form
+        submitButton.setOnClickListener(view -> submitFeedback());
+    }
+
+    private void submitFeedback() {
+        // Get the user to write feedback
+        String feedbackComment = commentEditText.getText().toString().trim();
+
+        if (!feedbackComment.isEmpty()) {
+
+            // Hide the feedback prompt and comment field
+            feedbackPromptTextView.setVisibility(View.GONE);
+            commentEditText.setVisibility(View.GONE);
+
+            // Display a toast message
+            Toast.makeText(this, "Feedback submitted successfully", Toast.LENGTH_SHORT).show();
+        } else {
+            // Display an error message if the comment is empty
+            Toast.makeText(this, "Please enter your feedback", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
