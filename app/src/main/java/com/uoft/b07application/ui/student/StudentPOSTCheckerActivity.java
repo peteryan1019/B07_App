574eebfe63c7f7df0a2993a2de5b58cd980a9b0d
Bomb3077 <yusheng3077@gmail.com>
2023-11-23 15:40:23 -0500

    finished menu in every pages and built a format for all activities

--- app/src/main/java/com/uoft/b07application/ui/student/StudentPOSTCheckerActivity.java
+++ app/src/main/java/com/uoft/b07application/ui/student/StudentPOSTCheckerActivity.java
@@ -6,11 +6,24 @@ import android.os.Bundle;
 
 import com.uoft.b07application.R;
 
-public class StudentPOSTCheckerActivity extends AppCompatActivity {
+public class StudentPOSTCheckerActivity extends StudentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_postchecker);
+    }
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_student_postchecker;
+    }
+    @Override
+    protected void setMenu(){
+        drawerLayout = findViewById(R.id.post_checker_drawer_layout);
+        navigationView = findViewById(R.id.nav_post_checker_view);
+        toolbar = findViewById(R.id.post_checker_toolbar);
+    }
+    @Override
+    public void setButtonListeners(){
+
     }
 }
--- app/src/main/java/com/uoft/b07application/ui/student/StudentProfileActivity.java
+++ app/src/main/java/com/uoft/b07application/ui/student/StudentProfileActivity.java
@@ -6,10 +6,23 @@ import android.os.Bundle;
 
 import com.uoft.b07application.R;
 
-public class StudentProfileActivity extends AppCompatActivity {
+public class StudentProfileActivity extends StudentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_profile);
+    }
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_student_profile;
+    }
+    @Override
+    protected void setMenu(){
+        drawerLayout = findViewById(R.id.student_profile_drawer_layout);
+        navigationView = findViewById(R.id.nav_student_profile_view);
+        toolbar = findViewById(R.id.student_profile_toolbar);
+    }
+    @Override
+    public void setButtonListeners(){
+
     }
 }
