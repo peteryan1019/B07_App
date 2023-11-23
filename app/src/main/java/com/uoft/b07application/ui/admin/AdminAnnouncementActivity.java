f7cb2333f3b7411d91d3074907f2a9cece6ab5af
Bomb3077 <yusheng3077@gmail.com>
2023-11-23 12:22:47 -0500

    finish menu bar for announcement page

--- app/src/main/java/com/uoft/b07application/ui/admin/AdminAnnouncementActivity.java
+++ app/src/main/java/com/uoft/b07application/ui/admin/AdminAnnouncementActivity.java
@@ -1,16 +1,28 @@
 package com.uoft.b07application.ui.admin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.uoft.b07application.R;
 
-public class AdminAnnouncementActivity extends AppCompatActivity {
+public class AdminAnnouncementActivity extends AdminActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_announcement);
     }
+    @Override
+    protected int setLayoutId(int layoutId) {
+        return R.layout.activity_admin_announcement;
+    }
+    @Override
+    protected void setAdminMenu(){
+        drawerLayout = findViewById(R.id.announcement_drawer_layout);
+        navigationView = findViewById(R.id.nav_announcement_view);
+        toolbar = findViewById(R.id.announcement_toolbar);
+    }
+    @Override
+    protected void setButtonListeners(){
+
+    }
+
 }
