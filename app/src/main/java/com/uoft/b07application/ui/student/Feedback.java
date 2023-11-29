4de4dd023bbc49bea305f6b0f133f739ca0db8c9
Ben Lu <lenbu@Bens-MacBook-Pro.local>
2023-11-29 14:16:20 -0500

    making sure

--- app/src/main/java/com/uoft/b07application/ui/student/Feedback.java
+++ app/src/main/java/com/uoft/b07application/ui/student/Feedback.java
@@ -1,2 +1,34 @@
-package com.uoft.b07application.ui.student;public class Feedback {
+package com.uoft.b07application.ui.student;
+
+public class Feedback {
+    private String name;
+    private String eventName;
+    private float rating;
+    private String feedbackComment;
+
+    // Default constructor required for Firebase
+    public Feedback() {}
+
+    public Feedback(String name, String eventName, float rating, String feedbackComment) {
+        this.name = name;
+        this.eventName = eventName;
+        this.rating = rating;
+        this.feedbackComment = feedbackComment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public String getFeedbackComment() {
+        return feedbackComment;
+    }
 }
