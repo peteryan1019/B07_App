5b27e3b5e727b792f2ef6abd6747c44a8f5d5644
Leo <leoyang388@gmail.com>
2023-12-02 21:08:26 -0500

    Fixed Mockito version to one that's compatible

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,8 @@ dependencies {
     implementation("androidx.annotation:annotation:1.6.0")
     implementation("com.google.firebase:firebase-messaging:23.3.1")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-all:1.10.19")
+    testImplementation("org.mockito:mockito-core:4.7.0")
+    //testImplementation("org.mockito:mockito-all:1.10.19")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/test/java/com/uoft/b07application/LoginPresenterUnitTest.java
+++ app/src/test/java/com/uoft/b07application/LoginPresenterUnitTest.java
@@ -3,9 +3,10 @@ package com.uoft.b07application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import android.text.Editable;
 import android.view.View;
@@ -14,7 +15,6 @@ import android.widget.EditText;
 import com.uoft.b07application.ui.login.LoginActivityModel;
 import com.uoft.b07application.ui.login.LoginActivity;
 import com.uoft.b07application.ui.login.LoginActivityPresenter;
-// Import other necessary classes and interfaces used in LoginActivityPresenter and its dependencies
 
 @RunWith(MockitoJUnitRunner.class)
 public class LoginPresenterUnitTest {
@@ -47,5 +47,47 @@ public class LoginPresenterUnitTest {
         LoginActivityPresenter presenter = new LoginActivityPresenter(mockLoginView, mockLoginModel);
         presenter.validateCredentials("username", "");
         verify(mockLoginView).setPasswordError("Password cannot be empty");
+    }
+
+    @Test
+    public void testAdminLogin() {
+        String adminUsername = "admin";
+        String adminPassword = "admin_password";
+
+        // Mock a successful login callback for an admin user
+        doAnswer(invocation -> {
+            LoginActivityModel.OnLoginFinishedListener listener = invocation.getArgument(2);
+            listener.onSuccess(adminUsername, "Admin Name", "admin@example.com", true);
+            return null;
+        }).when(mockLoginModel).loginUser(eq(adminUsername), eq(adminPassword), any(LoginActivityModel.OnLoginFinishedListener.class));
+
+        // Trigger validateCredentials
+        LoginActivityPresenter presenter = new LoginActivityPresenter(mockLoginView, mockLoginModel);
+        presenter.validateCredentials(adminUsername, adminPassword);
+
+        // Verify interactions: Expect the view to navigate to the admin screen
+        verify(mockLoginView).navigateToAdminScreen(eq(adminUsername), anyString(), anyString());
+        verifyNoMoreInteractions(mockLoginView);
+    }
+
+    @Test
+    public void testStudentLogin() {
+        String studentUsername = "student";
+        String studentPassword = "student_password";
+
+        // Mock a successful login callback for a student user
+        doAnswer(invocation -> {
+            LoginActivityModel.OnLoginFinishedListener listener = invocation.getArgument(2);
+            listener.onSuccess(studentUsername, "Student Name", "student@example.com", false);
+            return null;
+        }).when(mockLoginModel).loginUser(eq(studentUsername), eq(studentPassword), any(LoginActivityModel.OnLoginFinishedListener.class));
+
+        // Trigger validateCredentials
+        LoginActivityPresenter presenter = new LoginActivityPresenter(mockLoginView, mockLoginModel);
+        presenter.validateCredentials(studentUsername, studentPassword);
+
+        // Verify interactions: Expect the view to navigate to the student screen
+        verify(mockLoginView).navigateToStudentScreen(eq(studentUsername), anyString(), anyString());
+        verifyNoMoreInteractions(mockLoginView);
     }
 }
